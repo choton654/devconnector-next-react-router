a833e99ed855fd9d38947421090fc4d938c9019d
Choton <choton654@gmail.com>
2020-10-15 20:08:13 +0530

    'add-url'

--- utils/baseurl.js
+++ utils/baseurl.js
@@ -1,6 +1,8 @@
 const baseUrl =
   process.env.NODE_ENV === "production"
-    ? "https://devconector-nextjs.choton654.vercel.app"
+    ? "https://rocky-crag-53197.herokuapp.com"
     : "http://localhost:3000";
 
 export default baseUrl;
+
+// "https://devconector-nextjs.choton654.vercel.app" ||
